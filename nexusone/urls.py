fe18548343f0e5b736a67afac6a9cdc57993c318
Planeacion2510 <aux5gyd@hotmail.com>
2025-10-16 10:54:46 -0500

    Creacion de Puente Ngrok – Render - PC

--- nexusone/urls.py
+++ nexusone/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from . import views
+import os
 
 urlpatterns = [
     # Página principal
@@ -21,8 +23,16 @@ urlpatterns = [
         "administrativa/",
         include(("nexusone.administrativa.urls", "administrativa"), namespace="administrativa")
     ),
+
+    # Servir PDFs desde la carpeta Ordenes
+    re_path(
+        r'^Ordenes/(?P<path>.*)$',
+        serve,
+        {'document_root': os.path.join(settings.MEDIA_ROOT, 'Ordenes')},
+        name='ordenes_files'
+    ),
 ]
 
+# Servir archivos media (opcional)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
