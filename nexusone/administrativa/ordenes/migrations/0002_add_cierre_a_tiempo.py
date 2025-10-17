daf1e0ce62ded3a8d3ff8075fb4fdef1bee4f09c
Planeacion2510 <aux5gyd@hotmail.com>
2025-10-17 12:06:07 -0500

    Fix: Resolver conflicto en migración 0002

--- nexusone/administrativa/ordenes/migrations/0002_add_cierre_a_tiempo.py
+++ nexusone/administrativa/ordenes/migrations/0002_add_cierre_a_tiempo.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Generated manually
 from django.db import migrations, models
 
@@ -20,27 +19,4 @@ class Migration(migrations.Migration):
             name='cierre_a_tiempo',
             field=models.BooleanField(blank=True, null=True, verbose_name='Cierre a Tiempo'),
         ),
-=======
-# Generated manually
-from django.db import migrations, models
-
-
-class Migration(migrations.Migration):
-
-    dependencies = [
-        ('ordenes', '0001_initial'),
-    ]
-
-    operations = [
-        migrations.AlterField(
-            model_name='ordentrabajo',
-            name='fecha_cierre',
-            field=models.DateTimeField(blank=True, null=True, verbose_name='Fecha de Cierre'),
-        ),
-        migrations.AddField(
-            model_name='ordentrabajo',
-            name='cierre_a_tiempo',
-            field=models.BooleanField(blank=True, null=True, verbose_name='Cierre a Tiempo'),
-        ),
->>>>>>> 7f28ea4b99583bb6e745f09358d3439ba5564e95
     ]
