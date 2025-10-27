f110eea7f3184eff1a4f99c6bd1ba00623118302
Planeacion2510 <aux5gyd@hotmail.com>
2025-10-27 11:26:34 -0500

    Fix import circular: Proveedor lazy reference

--- nexusone/administrativa/inventario/models.py
+++ nexusone/administrativa/inventario/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.utils import timezone
 from datetime import date
-from nexusone.administrativa.compras.models import Proveedor  # 🔗 importamos
-
 # ---------------------------
 # INSUMOS
 # ---------------------------
